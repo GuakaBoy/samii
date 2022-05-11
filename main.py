39fb22cbc3eb447ea48ea89d319b41759e5e0e51
GuakaBoy <105384675+GuakaBoy@users.noreply.github.com>
2022-05-11 20:59:37 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ def on_button_pressed_a():
     basic.show_icon(IconNames.HEART)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_ab():
+    basic.show_icon(IconNames.CHESSBOARD)
+    basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+
 def on_button_pressed_b():
-    for indeks in range(3):
+    for indeks2 in range(3):
         music.play_tone(262, music.beat(BeatFraction.QUARTER))
-        basic.show_number(1 + indeks)
+        basic.show_number(1 + indeks2)
     music.play_tone(392, music.beat(BeatFraction.WHOLE))
     basic.show_string("GO!")
     basic.show_icon(IconNames.HEART)
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
 
 basic.show_icon(IconNames.HEART)
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-    for (let indeks = 0; indeks <= 2; indeks++) {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    for (let indeks = 0; indeks < 3; indeks++) {
         music.playTone(262, music.beat(BeatFraction.Quarter))
         basic.showNumber(3 - indeks)
     }
@@ -7,8 +7,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("GO!")
     basic.showIcon(IconNames.Heart)
 })
-input.onButtonPressed(Button.B, function () {
-    for (let indeks2 = 0; indeks2 <= 2; indeks2++) {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    for (let indeks2 = 0; indeks2 < 3; indeks2++) {
         music.playTone(262, music.beat(BeatFraction.Quarter))
         basic.showNumber(1 + indeks2)
     }
